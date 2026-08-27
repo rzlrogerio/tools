522eac4be442d21783f6028fe6cd27468f0646d6
Rogério de Araújo Rodrigues <rzlrogerio@gmail.com>
2026-08-27 17:41:25 -0300

    feat: trocando o hstr para o tv

--- zsh/rc/zsh/03-common.zsh
+++ zsh/rc/zsh/03-common.zsh
@@ -18,6 +18,8 @@ setopt HIST_IGNORE_ALL_DUPS
 setopt HIST_IGNORE_SPACE
 setopt HIST_FIND_NO_DUPS
 setopt HIST_SAVE_NO_DUPS
+unsetopt EXTENDED_HISTORY
+
 
 # ============================================================================
 # Completion System
